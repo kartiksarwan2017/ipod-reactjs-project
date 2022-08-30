8a3a6f2c2bdd1b246e6f97c71e1df48f84048354
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-08-30 09:08:18 +0530

    first commit

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./Lakshya.mp3";
+import music2 from "./Hall_of_Fame.mp3";
+import music3 from "./Woh_Kisna_Hai.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
